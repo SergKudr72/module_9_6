f359b1043c89458c2c995d160bc22a8f868db0b2
SergKudr72 <savat28@yandex.ru>
2024-10-25 00:02:49 +0500

    Add files via upload

---
+++ module_9_6.py
@@ -0,0 +1,13 @@
+def all_variants(text):
+    for x in range(len(text)):
+        for y in range(len(text) - x):
+            yield text[y:y+x+1]
+
+
+a = all_variants("abc")
+for i in a:
+    print(i)
+
+# print(a)
+# print(tuple(all_variants('qwerty')))
+
